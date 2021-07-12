03f9db5fbbbc794ba5cc2cfc6a18ca7016d43d2e
planteatre <amogh1216@gmail.com>
2021-07-11 19:02:11 -0700

    block gripper subassembly, todo constants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/BlockGripper.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class BlockGripper {
+    public Servo blockGripper;
+    public Servo blockRotator;
+
+    // TODO tune constants
+    public enum Position {
+        READY(0),
+        GRAB(70),
+        RELEASE(-20);
+
+        public double gripperPos;
+
+        Position(double gripperPos){
+            this.gripperPos = gripperPos;
+        }
+    }
+
+    public double initPos = Position.READY.gripperPos;
+
+    public BlockGripper(DeviceManager deviceManager){
+        blockGripper = deviceManager.blockGripper;
+        blockGripper.setPosition(initPos);
+
+        // not going to be using
+        blockRotator = deviceManager.blockRotator;
+    }
+
+    public void runGripper(Position position){
+        blockGripper.setPosition(position.gripperPos);
+    }
+
+    public void ready(){
+        runGripper(Position.READY);
+    }
+
+    public void grab(){
+        runGripper(Position.GRAB);
+    }
+
+    public void release(){
+        runGripper(Position.RELEASE);
+    }
+}
