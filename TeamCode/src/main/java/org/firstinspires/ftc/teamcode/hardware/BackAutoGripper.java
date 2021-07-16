f7b6acdf2cd1875df763c23e13c68b95c23645c5
bhargavi-suresh <bhargavividhulasuresh@gmail.com>
2021-07-15 17:53:43 -0700

    created the BackAutoGripper class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/BackAutoGripper.java
@@ -0,0 +1,12 @@
+    package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class BackAutoGripper {
+    public Servo backJoint;
+    public Servo backElbow;
+
+    public BackAutoGripper(DeviceManager deviceManager){
+
+    }
+}
